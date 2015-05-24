47dd32572504418f97d6e8fb879fd0f37480a76d
felixHPatzschke <felix.patzschke@gmail.com>
2015-05-24 19:56:18 +0200

    Adjusted the integration code
    For an integration interval [a;b] where b<a the former code returned 0
    for all three integration methods.

--- include/Integration.h
+++ include/Integration.h
@@ -4,13 +4,21 @@
 
 namespace phs
 {
+	/**
+	 * ints that specify integration methods
+	 */
+	unsigned short live_integration_method=0, cache_integration_method=0;
+	unsigned const short PHS_INTEGRATION_EULER_FORWARD=0, PHS_INTEGRATION_EULER_BACKWARD=1, PHS_INTEGRATION_EULER_TRIANGLES=2; 
 
+	/**
+	 * specific integration methods
+	 */
 	double integrate_eulerForward(double(*integrand)(double), double a, double b, unsigned int precision)
 	{
 		double dx = b-a;
 		dx /= precision;
 		double integral = 0;
-		for(double x=a; x<b-(dx/2.0); x+=dx)
+		for( double x=a; a<=x&&x<b-(dx/2.0) || a>=x&&x>b+(dx/2.0); x+=dx )
 		{
 			integral += dx*integrand(x);
 		}
@@ -22,7 +30,7 @@ namespace phs
 		double dx = b-a;
 		dx /= precision;
 		double integral = 0;
-		for(double x=a+dx; x<b+(dx/2.0); x+=dx)
+		for( double x=a+dx; a+dx<=x&&x<b+(dx/2.0) || a+dx>=x&&x>b-(dx/2.0); x+=dx )
 		{
 			integral += dx*integrand(x);
 		}
@@ -34,11 +42,64 @@ namespace phs
 		double dx = b-a;
 		dx /= precision;
 		double integral = 0;
-		for(double x=a; x<b-(dx/2.0); x+=dx)
+		if(dx>0.0)
 		{
-			integral += dx*(integrand(x)+((integrand(x+dx)-integrand(x))/2.0));
+			for( double x=a; x<b-(dx/2.0); x+=dx )
+			{
+				integral += dx*(integrand(x)+((integrand(x+dx)-integrand(x))/2.0));
+			}
+		}else if(dx<0.0)
+		{
+			for( double x=a; x>b+(dx/2.0); x+=dx )
+			{
+				integral += dx*(integrand(x)-((integrand(x+dx)-integrand(x))/2.0));
+			}
+		}else
+		{
+			//dx should really not be 0
 		}
+		
 		return integral;
+	}
+
+
+	/**
+	 * Non-specific integration methods
+	 */
+	double live_integrate(double(*f)(double), double a, double b, unsigned int p)
+	{
+		switch(live_integration_method)
+		{
+		case PHS_INTEGRATION_EULER_FORWARD:
+			return integrate_eulerForward(f,a,b,p);
+			break;
+		case PHS_INTEGRATION_EULER_BACKWARD:
+			return integrate_eulerBackward(f,a,b,p);
+			break;
+		case PHS_INTEGRATION_EULER_TRIANGLES:
+			return integrate_eulerTriangles(f,a,b,p);
+			break;
+		default:
+			return 0;
+		}
+	}
+
+	double cache_integrate(double(*f)(double), double a, double b, unsigned int p)
+	{
+		switch(cache_integration_method)
+		{
+		case PHS_INTEGRATION_EULER_FORWARD:
+			return integrate_eulerForward(f,a,b,p);
+			break;
+		case PHS_INTEGRATION_EULER_BACKWARD:
+			return integrate_eulerBackward(f,a,b,p);
+			break;
+		case PHS_INTEGRATION_EULER_TRIANGLES:
+			return integrate_eulerTriangles(f,a,b,p);
+			break;
+		default:
+			return 0;
+		}
 	}
 
 }
