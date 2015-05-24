a52ed4abf1415bd60ece38424232c104bc11ecda
felixHPatzschke <felix.patzschke@gmail.com>
2015-05-25 00:03:38 +0200

    Added Vector integration
    Added functions for integration of vector functions (same procedure as
    in the scalar integration). Also, changes the
    scalar-triangle-integration-algorithm and removed the non-specific
    integration for now.

--- include/Integration.h
+++ include/Integration.h
@@ -4,19 +4,59 @@
 
 namespace phs
 {
+	
 	/**
-	 * ints that specify integration methods
+	 * vector integration methods
 	 */
-	unsigned short live_integration_method=0, cache_integration_method=0;
-	unsigned const short PHS_INTEGRATION_EULER_FORWARD=0, PHS_INTEGRATION_EULER_BACKWARD=1, PHS_INTEGRATION_EULER_TRIANGLES=2; 
+	Vector integrate_eulerForward(Vector(*integrand)(Vector), Vector a, Vector b, unsigned int precision)
+	{
+		Vector dx = b-a*(1.0/precision);
+		dx *= (1.0/precision);
+		Vector integral = Vector(0,0,0);
+		Vector arg = a;
+		integral += integrand(arg)*(dx.abs());
+		for(unsigned int c=1; c<precision; c++)
+		{
+			arg += dx;
+			integral += integrand(arg)*(dx.abs());
+		}
+		return integral;
+	}
+
+	Vector integrate_eulerBackward(Vector(*integrand)(Vector), Vector a, Vector b, unsigned int precision)
+	{
+		Vector dx = b-a*(1.0/precision);
+		dx *= (1.0/precision);
+		Vector integral = Vector(0,0,0);
+		Vector arg = a;
+		for(unsigned int c=0; c<precision; c++)
+		{
+			arg += dx;
+			integral += integrand(arg)*(dx.abs());
+		}
+		return integral;
+	}
+
+	Vector integrate_eulerTriangles(Vector(*integrand)(Vector), Vector a, Vector b, unsigned int precision)
+	{
+		Vector dx = b-a*(1.0/precision);
+		Vector integral = Vector(0,0,0);
+		Vector arg = a;
+		integral += (integrand(arg)+(integrand(arg+dx)-integrand(arg))*0.5)*(dx.abs());
+		for(unsigned int c=1; c<precision; c++)
+		{
+			arg += dx;
+			integral += (integrand(arg)+(integrand(arg+dx)-integrand(arg))*0.5)*(dx.abs());
+		}
+		return integral;
+	}
 
 	/**
-	 * specific integration methods
+	 * double integration methods
 	 */
 	double integrate_eulerForward(double(*integrand)(double), double a, double b, unsigned int precision)
 	{
-		double dx = b-a;
-		dx /= precision;
+		double dx = dx = (b-a)/precision;
 		double integral = 0;
 		for( double x=a; a<=x&&x<b-(dx/2.0) || a>=x&&x>b+(dx/2.0); x+=dx )
 		{
@@ -27,8 +67,8 @@ namespace phs
 
 	double integrate_eulerBackward(double(*integrand)(double), double a, double b, unsigned int precision)
 	{
-		double dx = b-a;
-		dx /= precision;
+		double dx = (b-a)/precision;
+
 		double integral = 0;
 		for( double x=a+dx; a+dx<=x&&x<b+(dx/2.0) || a+dx>=x&&x>b-(dx/2.0); x+=dx )
 		{
@@ -39,14 +79,22 @@ namespace phs
 
 	double integrate_eulerTriangles(double(*integrand)(double), double a, double b, unsigned int precision)
 	{
-		double dx = b-a;
-		dx /= precision;
+		double dx = (b-a)/precision;
 		double integral = 0;
+		double x = a;
+
+		integral += dx*(integrand(x)+((integrand(x+dx)-integrand(x))/2.0));
+		for(unsigned int c=1; c<precision; c++)
+		{
+			x += dx;
+			integral += dx*(integrand(x)+((integrand(x+dx)-integrand(x))/2.0));
+		}
+		/*
 		if(dx>0.0)
 		{
 			for( double x=a; x<b-(dx/2.0); x+=dx )
 			{
-				integral += dx*(integrand(x)+((integrand(x+dx)-integrand(x))/2.0));
+				
 			}
 		}else if(dx<0.0)
 		{
@@ -58,48 +106,8 @@ namespace phs
 		{
 			//dx should really not be 0
 		}
-		
+		*/
 		return integral;
-	}
-
-
-	/**
-	 * Non-specific integration methods
-	 */
-	double live_integrate(double(*f)(double), double a, double b, unsigned int p)
-	{
-		switch(live_integration_method)
-		{
-		case PHS_INTEGRATION_EULER_FORWARD:
-			return integrate_eulerForward(f,a,b,p);
-			break;
-		case PHS_INTEGRATION_EULER_BACKWARD:
-			return integrate_eulerBackward(f,a,b,p);
-			break;
-		case PHS_INTEGRATION_EULER_TRIANGLES:
-			return integrate_eulerTriangles(f,a,b,p);
-			break;
-		default:
-			return 0;
-		}
-	}
-
-	double cache_integrate(double(*f)(double), double a, double b, unsigned int p)
-	{
-		switch(cache_integration_method)
-		{
-		case PHS_INTEGRATION_EULER_FORWARD:
-			return integrate_eulerForward(f,a,b,p);
-			break;
-		case PHS_INTEGRATION_EULER_BACKWARD:
-			return integrate_eulerBackward(f,a,b,p);
-			break;
-		case PHS_INTEGRATION_EULER_TRIANGLES:
-			return integrate_eulerTriangles(f,a,b,p);
-			break;
-		default:
-			return 0;
-		}
 	}
 
 }
